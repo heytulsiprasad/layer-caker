6b758bbe121b4e06421d2dc7d3feb5443eee74c6
heytulsiprasad <tulsi.prasad50@gmail.com>
2025-05-12 14:54:29 +0530

    Setup draft previews

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,8 +1,11 @@
 import "@/app/globals.css";
+import { DisableDraftMode } from "@/components/DisableDraftMode";
 import { Header } from "@/components/Header";
 import { SanityLive } from "@/sanity/lib/live";
+import { VisualEditing } from "next-sanity";
+import { draftMode } from "next/headers";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -13,6 +16,12 @@ export default function RootLayout({
         <Header />
         {children}
         <SanityLive />
+        {(await draftMode()).isEnabled && (
+          <>
+            <DisableDraftMode />
+            <VisualEditing />
+          </>
+        )}
       </body>
     </html>
   );
