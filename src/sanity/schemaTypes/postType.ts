8f067944910e4a7cb7fecf7f1025996375e02fd0
heytulsiprasad <tulsi.prasad50@gmail.com>
2025-05-12 12:32:37 +0530

    Add interesting fields to post type

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -1,65 +1,134 @@
-import {DocumentTextIcon} from '@sanity/icons'
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { DocumentTextIcon } from "@sanity/icons";
+import { defineArrayMember, defineField, defineType } from "sanity";
 
 export const postType = defineType({
-  name: 'post',
-  title: 'Post',
-  type: 'document',
+  name: "post",
+  title: "Post",
+  type: "document",
   icon: DocumentTextIcon,
+  groups: [
+    { name: "content", title: "Content" },
+    { name: "settings", title: "Settings" },
+  ],
   fields: [
     defineField({
-      name: 'title',
-      type: 'string',
+      name: "title",
+      type: "string",
+      group: ["content", "settings"],
     }),
     defineField({
-      name: 'slug',
-      type: 'slug',
+      name: "slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
       },
+      validation: (rule) => rule.required().error(`I said required`),
+      group: "content",
     }),
     defineField({
-      name: 'author',
-      type: 'reference',
-      to: {type: 'author'},
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
+      group: "content",
     }),
     defineField({
-      name: 'mainImage',
-      type: 'image',
+      name: "writingSeason",
+      type: "string",
+      options: {
+        list: ["spring", "summer", "autumn", "winter"],
+        layout: "radio",
+      },
+      group: "settings",
+    }),
+    defineField({
+      name: "writingMonth",
+      type: "string",
+      options: {
+        list: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        layout: "dropdown",
+      },
+      group: "settings",
+      validation: (rule) => {
+        return rule.custom((value, context) => {
+          if (value) {
+            const seasonsWithMonths = {
+              spring: ["March", "April", "May"],
+              summer: ["June", "July", "August"],
+              autumn: ["September", "October", "November"],
+              winter: ["December", "January", "February"],
+            };
+
+            const season = context.document?.writingSeason;
+            if (
+              season &&
+              !seasonsWithMonths[
+                season as keyof typeof seasonsWithMonths
+              ].includes(value)
+            ) {
+              return "Invalid month for the selected season";
+            }
+          }
+
+          return true;
+        });
+      },
+      hidden: ({ document }) => !document?.writingSeason,
+    }),
+    defineField({
+      name: "mainImage",
+      type: "image",
       options: {
         hotspot: true,
       },
+      group: "content",
       fields: [
         defineField({
-          name: 'alt',
-          type: 'string',
-          title: 'Alternative text',
-        })
-      ]
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+          group: "settings",
+        }),
+      ],
     }),
     defineField({
-      name: 'categories',
-      type: 'array',
-      of: [defineArrayMember({type: 'reference', to: {type: 'category'}})],
+      name: "categories",
+      type: "array",
+      of: [defineArrayMember({ type: "reference", to: { type: "category" } })],
+      group: "content",
     }),
     defineField({
-      name: 'publishedAt',
-      type: 'datetime',
+      name: "publishedAt",
+      type: "datetime",
+      group: "content",
     }),
     defineField({
-      name: 'body',
-      type: 'blockContent',
+      name: "body",
+      type: "blockContent",
+      group: "content",
     }),
   ],
   preview: {
     select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
     },
     prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
     },
   },
-})
+});
