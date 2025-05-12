e08df32748563b6dff63c08db66a874a233b10d5
heytulsiprasad <tulsi.prasad50@gmail.com>
2025-05-12 13:10:56 +0530

    Add new pages structure

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,17 @@
+import "@/app/globals.css";
+import { SanityLive } from "@/sanity/lib/live";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <SanityLive />
+      </body>
+    </html>
+  );
+}
